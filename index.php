0defded60eea3945cd9de057a7a57f87db97c1e6
Guillermoperez201704664-lgtm <guillermousacpem@gmail.com>
2020-11-16 00:57:52 -0600

    Serie

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>  Caratula  </title>
+</head>
+<body>
+<meta charset="utf-8">
+<hr>
+<center>
+<br>
+<br>
+<table border="2" width="90%">
+	<tbody>
+		<tr>
+			<td colspan="3"> <center> Informacion General  </center> </td> 
+		</tr>
+		<tr>
+			<td> Nombre:</td>
+			<td> Guillermo Esteban Perez Sanchez</td>
+			<td rowspan="3"> <center><img width="30%" src="PaginaWeb2/imagencaratula/usac.jpg"></center>
+			</td>
+		</tr>
+		<tr>
+		  <td> Fecha </td>
+		  <td> 16/11/2020 </td>
+		 </tr>
+		 <tr>
+		  <td> Carnet </td>
+		  <td> 201704664 </td>
+		 </tr>
+	</tbody>
+</table>
+<br>
+<br>
+<br>
+<table>
+<tbody>
+    <tr>
+    <td>
+	 <center> Enlaces</center>
+	 </td>
+	 <br>
+	 </tr>
+
+	 <tr>
+	 	<td>
+	 		<ul>
+	 			<li> <a href="PaginaWeb2/pagina2.html">Pagina 2 </a> </li>
+	 			<li> <a href="PaginaWeb2/pagina3.html"> Pagina 3 </a></li>
+	 		</ul>
+	 	</td>
+	 	 <br>
+	 	
+	 </tr>
+</tbody>
+</table>
+</center>
+<hr>
+<br>
+<br>
+</body>
+</html>
